2e7251cb87fa7c6d8c6d56374a200db451ff4e59
Wiktor <wiko700@gmail.com>
2025-07-09 19:54:05 +0200

    feat: improve article card appearance and adapt it to appearance of rest components

--- components/ArticleCard.tsx
+++ components/ArticleCard.tsx
@@ -14,33 +14,35 @@ interface ArticleCardProps {
 const ArticleCard: FC<ArticleCardProps> = ({ id, title, date, categories }) => {
   return (
     <motion.div 
-      className="rounded-2xl overflow-hidden bg-zinc-900 shadow-lg border border-zinc-700"
       initial={{ opacity: 0, x: 50 }}
       whileInView={{ opacity: 1, x: 0 }}
       transition={{ duration: 0.8, ease: "easeOut" }}
       viewport={{ once: true, amount: 0.2 }}
     >
-      <div className="p-4 space-y-3">
-        <div>
-          <a href={id} target="_blank" rel="noopener noreferrer" className="">
-            <h3 className="mb-0 text-xl font-semibold text-white hover:text-orange-500 transition duration-450">{title}</h3>
-          </a>
-        </div>
+      <a
+        href={id} 
+        target="_blank" 
+        rel="noopener noreferrer"
+        className="flex items-center h-full gap-4 p-4 rounded-xl bg-zinc-900 border border-zinc-700 hover:border-orange-500 transition-all duration-300 hover:shadow-lg group"
+      >
+        <div className="p-4 space-y-3">
+          <h3 className="text-xl font-semibold text-white">{title}</h3>
 
-        <div className="flex flex-wrap gap-2">
-            {categories.map((category: string) => (
-            <span
-                key={category}
-                className="text-xs font-medium px-2.5 py-0.5 rounded bg-orange-500 text-natural"
-            >
-                #{category}
-            </span>
-            ))}
-        </div>
-        
-        <p className="text-gray-400 text-sm">{moment(date, "DD-MM-YYYY").format("Do MMMM YYYY")}</p>
+          <div className="flex flex-wrap gap-2">
+              {categories.map((category: string) => (
+              <span
+                  key={category}
+                  className="text-xs font-medium px-2.5 py-0.5 rounded bg-orange-500 text-natural"
+              >
+                  #{category}
+              </span>
+              ))}
+          </div>
+          
+          <p className="text-gray-400 text-sm">{moment(date, "DD-MM-YYYY").format("Do MMMM YYYY")}</p>
 
-      </div>
+        </div>
+      </a>
     </motion.div>
   )
 }

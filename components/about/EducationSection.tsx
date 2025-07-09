86b6ae008a4b1c6cb58c9307544d4fd25fdf48c0
Wiktor <wiko700@gmail.com>
2025-07-09 19:58:32 +0200

    fix: remove unnecessary height property

--- components/about/EducationSection.tsx
+++ components/about/EducationSection.tsx
@@ -95,7 +95,7 @@ export default function EducationSection() {
   return (
     <motion.div
       ref={sectionRef}
-      className="h-[70vh] w-full relative"
+      className="w-full relative"
       initial={{ opacity: 0, x: 50 }}
       whileInView={{ opacity: 1, x: 0 }}
       transition={{ duration: 0.8, ease: "easeOut" }}

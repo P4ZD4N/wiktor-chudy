a51c7449fcd8f3be4ab524cb2d2531f3d3caa0a6
Wiktor <wiko700@gmail.com>
2025-07-15 22:18:51 +0200

    feat: add pagination to article pag

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -2,6 +2,7 @@ import ArticleCard from "@/components/articles/ArticleCard";
 import HorizontalArticleFilter from "@/components/articles/HorizontalArticleFilter";
 import FadeInOnScroll from "@/components/home/FadeInOnScroll";
 import { getCategoriesArticles } from "@/lib/articles";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 interface ArticlesPageProps {
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
@@ -43,6 +44,13 @@ export default async function ArticlesPage({
         })
     : [];
 
+
+    const page = parseInt(params.page as string) || 1;
+  const ARTICLES_PER_PAGE = 12;
+  const startIndex = (page - 1) * ARTICLES_PER_PAGE;
+  const endIndex = startIndex + ARTICLES_PER_PAGE;
+  const paginatedArticles = flatArticles.slice(startIndex, endIndex);
+
   return (
     <section className="relative mx-auto w-10/12 md:w-3/4 lg:w-2/3 xl:w-8/12 mt-20 flex flex-col gap-16 mb-20">
       <div className="fade-in fixed top-1/2 lg:left-1/80 left-1/4 lg:w-96 lg:h-96 w-64 h-64 bg-orange-500/15 rounded-full -translate-x-1/2 -translate-y-1/2 filter blur-3xl pointer-events-none z-0" />
@@ -64,7 +72,7 @@ export default async function ArticlesPage({
       </FadeInOnScroll>
 
       <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-8">
-        {flatArticles.map((article) => (
+        {paginatedArticles.map((article) => (
           <ArticleCard
             key={article.id}
             id={article.id}
@@ -74,6 +82,41 @@ export default async function ArticlesPage({
           />
         ))}
       </div>
+
+      <div className="flex justify-center mt-10 gap-4">
+        {page > 1 && (
+          <FadeInOnScroll direction="right" threshold={0.2}>
+            <a
+              href={`?${new URLSearchParams({
+              ...params,
+              page: (page - 1).toString(),
+              categories: selectedCategories.join(','),
+              }).toString()}`}
+            >
+              <div className="w-10 h-10 rounded-full bg-white/50 hover:bg-orange-500 shadow flex items-center justify-center transition-all duration-200">
+                <ChevronLeft className="text-black" />
+              </div>
+            </a>
+          </FadeInOnScroll>
+        )}
+
+        {flatArticles.length > endIndex && (
+          <FadeInOnScroll direction="left" threshold={0.2}>
+            <a
+              href={`?${new URLSearchParams({
+                ...params,
+                page: (page + 1).toString(),
+                categories: selectedCategories.join(','),
+              }).toString()}`}
+            >
+              <div className="w-10 h-10 rounded-full bg-white/50 hover:bg-orange-500 shadow flex items-center justify-center transition-all duration-200">
+                <ChevronRight className="text-black" />
+              </div>
+            </a>
+          </FadeInOnScroll>
+        )}
+      </div>
+
     </section>
   );
 }

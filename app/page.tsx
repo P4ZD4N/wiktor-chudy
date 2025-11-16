0060f6e29fff95eaf614808d48526a0530fa1f46
Wiktor <wiko700@gmail.com>
2025-11-16 19:59:36 +0100

    refactor: exclude HomeContact component from home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
-import ContactSection from "@/components/common/ContactSection";
-import FadeInOnScroll from "@/components/common/animations/FadeInOnScroll";
 import Hero from "@/components/home/Hero";
 import HomeProjects from "@/components/home/HomeProjects";
 import HomeArticles from "@/components/home/HomeArticles";
+import HomeContact from "@/components/home/HomeContact";
 
 const HomePage = async () => {
   return (
@@ -10,21 +9,7 @@ const HomePage = async () => {
       <Hero />
       <HomeProjects />
       <HomeArticles />
-
-      <section className="bg-neutral-950">
-        <section className="mt-20 mx-auto w-10/12 md:w-3/4 lg:w-2/3 xl:w-7/12 pb-20 w-full w-6/6 flex flex-col">
-          <FadeInOnScroll direction="right" threshold={0.2}>
-            <h2 className="mb-8 text-center text-3xl font-bold leading-none tracking-tight md:text-3xl lg:text-4xl text-white">
-              Contact{" "}
-              <span className="underline underline-offset-3 decoration-6 decoration-orange-400 dark:decoration-orange-500">
-                me
-              </span>
-            </h2>
-          </FadeInOnScroll>
-
-          <ContactSection />
-        </section>
-      </section>
+      <HomeContact />
     </section>
   );
 };
---
+++ components/home/HomeContact.tsx
@@ -0,0 +1,24 @@
+import FadeInOnScroll from "../common/animations/FadeInOnScroll";
+import ContactSection from "../common/ContactSection";
+
+const HEADER_FIRST_PART = "Contact ";
+const HEADER_SECOND_PART = "me"
+
+export default function HomeContact() {
+  return (
+    <section className="bg-neutral-950">
+      <section className="mt-20 mx-auto w-10/12 md:w-3/4 lg:w-2/3 xl:w-7/12 pb-20 w-full w-6/6 flex flex-col">
+        <FadeInOnScroll direction="right" threshold={0.2}>
+          <h2 className="mb-8 text-center text-3xl font-bold leading-none tracking-tight md:text-3xl lg:text-4xl text-white">
+            {HEADER_FIRST_PART}
+            <span className="underline underline-offset-3 decoration-6 decoration-orange-400 dark:decoration-orange-500">
+              {HEADER_SECOND_PART}
+            </span>
+          </h2>
+        </FadeInOnScroll>
+
+        <ContactSection />
+      </section>
+    </section>
+  );
+}

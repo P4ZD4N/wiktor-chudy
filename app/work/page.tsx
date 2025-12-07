9d01b4cf3aca998121bb21045d9e700a06eb4949
Wiktor <wiko700@gmail.com>
2025-12-07 17:24:28 +0100

    feat: add project url param in work page

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,92 +1,28 @@
-"use client";
-
-import { useState } from "react";
 import ProjectSection from "@/components/work/ProjectSection";
 import projects from "@/lib/projects";
-import GentlePop from "@/components/common/animations/GentlePop";
+import ProjectList from "@/components/work/ProjectList";
 
 const HEADER = "Projects";
 const PARAGRAPH = `My work is not just about coding. It’s also about understanding the
   needs of different businesses, which is shown by the projects I’ve
   completed for clients in many industries.`;
-const FIRST_BUTTON = "Projects list";
-const SECOND_BUTTON = "X";
 
-export default function WorkPage() {
-  const [selectedProject, setSelectedProject] = useState(projects[0]);
-  const [openSidebar, setOpenSidebar] = useState(false);
+type Params = Promise<{ project: string }>
 
+export default async function WorkPage(props: { searchParams: Params }) {
+  const params = await props.searchParams;
+  const selectedProjectTitle = params.project || projects[0].title;
+  const selectedProject =
+    projects.find((p) => p.title === selectedProjectTitle) || projects[0];
+    
   return (
     <section className="grid grid-cols-1 xl:grid-cols-3 min-h-screen xl:h-screen">
-      <button
-        className="xl:hidden w-full p-4 bg-orange-500 text-white text-xl font-bold"
-        onClick={() => setOpenSidebar((prev) => !prev)}
-      >
-        {FIRST_BUTTON}
-      </button>
-
-      <div
-        className={`
-          bg-neutral-950 flex flex-col gap-4 overflow-y-auto scrollbar-hidden
-          p-10 md:p-20
-
-          xl:col-span-1 xl:relative xl:block
-          
-          fixed left-0 top-0 w-full h-full z-20
-          transform transition-transform duration-300 ease-in-out 
-          ${openSidebar ? "translate-y-0" : "-translate-y-full"}
-          
-          xl:translate-y-0
-        `}
-      >
-        <GentlePop>
-          <button
-            className="xl:hidden w-full px-3 py-1 bg-orange-500 text-white font-bold rounded-md"
-            onClick={() => setOpenSidebar(false)}
-          >
-            {SECOND_BUTTON}
-          </button>
-          <h1 className="text-4xl font-bold mt-4 xl:mt-0 mb-4 text-center underline underline-offset-3 decoration-6 decoration-orange-500">
-            {HEADER}
-          </h1>
-          <p className="hidden xl:block text-lg text-neutral-400 text-center mb-6">
-            {PARAGRAPH}
-          </p>
-        </GentlePop>
-
-        <ul className="flex flex-col gap-3">
-          {projects.map((project, index) => {
-            const listItem = (
-              <li
-                key={project.title}
-                className={`cursor-pointer py-2 px-3 rounded-lg transform transition-all duration-200
-                ${
-                  selectedProject.title === project.title
-                    ? "bg-orange-500 text-white font-bold shadow-lg scale-105"
-                    : "bg-neutral-900 hover:bg-neutral-800 hover:scale-105 hover:shadow-md"
-                }`}
-                onClick={() => {
-                  setSelectedProject(project);
-                  setOpenSidebar(false);
-                }}
-              >
-                <div className="flex flex-col">
-                  <span className="text-white font-bold">{project.title}</span>
-                  <span className="italic">{project.industry}</span>
-                </div>
-              </li>
-            );
-
-            if (openSidebar) return <div key={project.title}>{listItem}</div>;
-
-            return (
-              <GentlePop key={project.title} delay={`${0.2 + index * 0.2}s`}>
-                {listItem}
-              </GentlePop>
-            );
-          })}
-        </ul>
-      </div>
+      <ProjectList
+        projects={projects}
+        selectedTitle={selectedProject.title}
+        header={HEADER}
+        paragraph={PARAGRAPH}
+      />
 
       <div className="col-span-2 p-10 md:p-20 h-full overflow-y-auto scrollbar-hidden">
         <ProjectSection key={selectedProject.title} {...selectedProject} />
@@ -94,3 +30,4 @@ export default function WorkPage() {
     </section>
   );
 }
+
---
+++ components/work/ProjectList.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import { useState } from "react";
+import GentlePop from "@/components/common/animations/GentlePop";
+import Link from "next/link";
+
+const FIRST_BUTTON = "Projects list";
+const SECOND_BUTTON = "X";
+
+export default function ProjectList({
+  projects,
+  selectedTitle,
+  header,
+  paragraph,
+}: {
+  projects: { title: string; industry: string }[];
+  selectedTitle: string;
+  header: string;
+  paragraph: string;
+}) {
+  const [openSidebar, setOpenSidebar] = useState(false);
+  const projectUrl = (title: string) => {
+    return `?project=${encodeURIComponent(title)}`;
+  };
+
+  return (
+    <>
+      <button
+        className="xl:hidden w-full p-4 bg-orange-500 text-white text-xl font-bold"
+        onClick={() => setOpenSidebar((prev) => !prev)}
+      >
+        {FIRST_BUTTON}
+      </button>
+
+      <div
+        className={`
+          bg-neutral-950 flex flex-col gap-4 overflow-y-auto scrollbar-hidden
+          p-10 md:p-20
+
+          xl:col-span-1 xl:relative xl:block
+          
+          fixed left-0 top-0 w-full h-full z-20
+          transform transition-transform duration-300 ease-in-out 
+          ${openSidebar ? "translate-y-0" : "-translate-y-full"}
+          
+          xl:translate-y-0
+        `}
+      >
+        <GentlePop>
+          <button
+            className="xl:hidden w-full px-3 py-1 bg-orange-500 text-white font-bold rounded-md"
+            onClick={() => setOpenSidebar(false)}
+          >
+            {SECOND_BUTTON}
+          </button>
+          <h1 className="text-4xl font-bold mt-4 xl:mt-0 mb-4 text-center underline underline-offset-3 decoration-6 decoration-orange-500">
+            {header}
+          </h1>
+          <p className="hidden xl:block text-lg text-neutral-400 text-center mb-6">
+            {paragraph}
+          </p>
+        </GentlePop>
+
+        <ul className="flex flex-col gap-3">
+          {projects.map((project, index) => {
+            const listItem = (
+              <Link
+                href={projectUrl(project.title)}
+                key={project.title}
+                className={`
+                  cursor-pointer py-2 px-3 rounded-lg transform transition-all duration-200 block
+                  ${
+                    selectedTitle === project.title
+                      ? "bg-orange-500 text-white font-bold shadow-lg scale-105"
+                      : "bg-neutral-900 hover:bg-neutral-800 hover:scale-105 hover:shadow-md"
+                  }
+                `}
+                onClick={() => setOpenSidebar(false)}
+              >
+                <div className="flex flex-col">
+                  <span className="text-white font-bold">{project.title}</span>
+                  <span className="italic">{project.industry}</span>
+                </div>
+              </Link>
+            );
+
+            if (openSidebar) return <div key={project.title}>{listItem}</div>;
+
+            return (
+              <GentlePop key={project.title} delay={`${0.2 + index * 0.2}s`}>
+                {listItem}
+              </GentlePop>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+}
